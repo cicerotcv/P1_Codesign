995148f438fa1403a5e4d6ebf8e312fd11755f6e
luizfelipelazzaron <luizfl@al.insper.edu.br>
2019-09-18 08:18:17 -0300

    Melhoria no Javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
   // Igrediente 2: Conectando os Valores no Firebase
   let db = coDesConnect('https://engineering-design-portfolio.firebaseio.com/')
   // Igrediente 1: lendo as chaves e os valores 
-  let data = coDesExtract()
-  for(let key in data) {
-    /*
-      O resultado deste loop pode ser visto no console.
-    */
-    console.log(data[key])
-  }
-  db.download('/', function(data) {
-  })
-  coDesReplace('.navbar_items', data)
-  coDesReplace('.category-buttons', data)
+  // let data = coDesExtract()
+  // for(let key in data) {
+  //   /*
+  //     O resultado deste loop pode ser visto no console.
+  //   */
+  //   console.log(data[key])
+  // }
 
+  db.download('/', function(data) {
+
+    // for(let key in data){
+    //   console.log(key)
+      console.log(data)
+      coDesReplace('.navbar_items', data)
+      coDesReplace('.category-buttons', data)
+    // }
+  })
 })
