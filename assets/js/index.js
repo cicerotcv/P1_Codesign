d442c778e304214be510cf2206d35fa8529d98f7
luizfelipelazzaron <luizfl@al.insper.edu.br>
2019-09-17 23:11:55 -0300

    Adições

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Igrediente 1: lendo as chaves e os valores 
+  let data= coDesExtract()
+  // Igrediente 2: Conectando os Valores no Firebase
   let db = coDesConnect('https://engineering-design-portfolio.firebaseio.com/')
 
-  db.download('/', function(data) {
+  db.download('/portfolio/fisicos', function(data) {
     context = data
     coDesReplace('.navbar_items', context)
+  
   })
 })
