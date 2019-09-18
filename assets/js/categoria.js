70e7c2aaf954069cbfeab0c2a3b8daf65030cd81
luizfelipelazzaron <luizfl@al.insper.edu.br>
2019-09-18 08:57:37 -0300

    kjwd

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -1,31 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let params = coDesExtract()
-  let value = params['key']
 
   let db = coDesConnect('https://engineering-design-portfolio.firebaseio.com/')
 
   db.download('/', function(data) {
+    //Navbar
+    // console.log(data)
+    coDesReplace('.navbar_items', data)
+    coDesReplace('.category-buttons', data)
+    //Título da Página
+    context = data['categoria'][value]
+    console.log(context)
+    coDesReplace('.call-title',data)
+    
 
-    /*
-      NUNCA, JAMAIS, EM HIPÓTESE ALGUMA faça
-      replace em múltiplas tags se alguma
-      estiver dentro de outra! Veja o HTML
-      e note que não é o caso das abaixo.
 
-      É sério. Se fizer isso, você realmente
-      vai se arrepender. Você foi avisado.
-    */
+    // context = data['biblioteca'][value]
+    // coDesReplace('title', context)
 
-    context = data['biblioteca'][value]
-    coDesReplace('title', context)
+    // context = data
+    // coDesReplace('.menu-list', context)
 
-    context = data
-    coDesReplace('.menu-list', context)
+    // context = data['biblioteca'][value]
+    // coDesReplace('.call', context)
 
-    context = data['biblioteca'][value]
-    coDesReplace('.call', context)
-
-    context = data['biblioteca'][value]
-    coDesReplace('.book-list', context)
+    // context = data['biblioteca'][value]
+    // coDesReplace('.book-list', context)
   })
 })
